fea89e9c7b0111fcc33d3b80fd97254721133477
sabz04 <zulfat.sabirov.04@bk.ru>
2023-06-14 11:34:25 +0300

    .

--- CryptocurrencyRateParserWfApp/Configuration/Configuration.cs
+++ CryptocurrencyRateParserWfApp/Configuration/Configuration.cs
@@ -8,20 +8,20 @@ namespace CryptocurrencyRateParserWfApp.Configuration
 {
     public static class ApiConfiguration
     {
-        public static string ApiKeyBybit = "ParBUwDl29ElWljUzO";
-        public static string ApiSecretBybit = "GjQVkBbWs8GKaJj8XpNRv1DH5ee2XIEZSwUu";
+        public static string ApiKeyBybit = "";
+        public static string ApiSecretBybit = "";
         /// <summary>
         /// api-ключи kucoin
         /// </summary>
-        public static string ApiKeyKucoin = "646f973a96076a0001c4bcae";
-        public static string ApiSecretKucoin = "c6b110f4-3025-46dd-874c-30021e09c1b0";
-        public static string ApiPassKucoin = "878278";
+        public static string ApiKeyKucoin = "";
+        public static string ApiSecretKucoin = "";
+        public static string ApiPassKucoin = "";
 
         /// <summary>
         /// api-ключи binance
         /// </summary>
-        public static string ApiKeyBinance = "bUdoFsQ05U9yOcp7APkAvQE9S54cuNTWt1jMjDMZxKSiNNeeKivmkzulPYnpY6DB";
-        public static string ApiSecretBinance = "URU6lMV5m42eoHfF8Oo41n5rwKG2CIaR3DGkB9DXNzVvwQjwbM0pjZxYGQJ73IP2";
+        public static string ApiKeyBinance = "";
+        public static string ApiSecretBinance = "";
 
     }
 }
